76db3b07bca709c3a9c030b2c9ec16fdb6c0b568
Simon Kornél <simon.kornel@mikrum.hu>
2019-10-05 10:49:06 +0200

    java 8

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,17 +1,24 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "All-in always";
+    static final String VERSION = "All-in always, jvm8";
 
     public static int betRequest(JsonElement request) {
         return Integer.MAX_VALUE;
     }
 
     public static void showdown(JsonElement game) {
+    }
+
+    private int[] getHand(JsonElement request) {
+    	
+    	int in_action = request.getAsJsonObject().get("in_action").getAsInt();
+    	return null;
     }
 }
