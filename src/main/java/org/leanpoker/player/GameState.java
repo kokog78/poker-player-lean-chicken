32fdda5643c3d0e0419ccb177f275a1925c42ad4
LaczkoUr <laczkour@gmail.com>
2019-10-05 13:56:30 +0200

    refactor start

--- src/main/java/org/leanpoker/player/GameState.java
+++ src/main/java/org/leanpoker/player/GameState.java
@@ -3,9 +3,9 @@ package org.leanpoker.player;
 import org.leanpoker.player.model.Card;
 import org.leanpoker.player.model.Game;
 import org.leanpoker.player.model.PlayerDto;
+import org.leanpoker.player.model.Suit;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class GameState {
 
@@ -74,8 +74,40 @@ public class GameState {
 		return true;
 	}
 
+	private Map<String, List<Card>> cardsMap(List<Card> cards) {
+		Map<String, List<Card>> rankMap = new HashMap<>();
+
+		cards.forEach(c -> {
+			if (!rankMap.containsKey(c.rank)) {
+				rankMap.put(c.rank, new ArrayList<Card>());
+			}
+			rankMap.get(c.rank).add(c);
+		});
+		return rankMap;
+	}
+
+	public boolean haveCards(List<Card> cards, boolean sameColor, String... rank) {
+		Map<String, List<Card>> rankMap = cardsMap(cards);
+		boolean clubs = true,spades = true,hearts = true,diamonds = true;
+		for(int i = 0; i < rank.length; i++) {
+			if (rankMap.get(rank[i]) == null || rankMap.get(rank[i]).size() == 0) {
+				return false;
+			} else {
+				clubs = clubs && rankMap.get(rank[i]).stream().anyMatch(c -> c.suit.equals(Suit.clubs));
+				spades = spades && rankMap.get(rank[i]).stream().anyMatch(c -> c.suit.equals(Suit.spades));
+				hearts = hearts && rankMap.get(rank[i]).stream().anyMatch(c -> c.suit.equals(Suit.hearts));
+				diamonds = diamonds && rankMap.get(rank[i]).stream().anyMatch(c -> c.suit.equals(Suit.diamonds));
+			}
+		}
+		if (sameColor) {
+			return clubs && spades && hearts && diamonds;
+		} else {
+			return true;
+		}
+	}
+
 	public boolean haveAce() {
-		return haveRankInHand("A");
+		return haveRankInAllCards("A");
 	}
 
 
---
+++ src/test/java/org/leanpoker/player/GameStateTest.java
@@ -0,0 +1,14 @@
+package org.leanpoker.player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameStateTest {
+
+    @Test
+    void testHaveCards() {
+        GameState gs = new GameState(null);
+
+    }
+}
